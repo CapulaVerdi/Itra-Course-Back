3dd5fc9d615d67945c05edb9c6ef8db926bd3754
Zakhar <zero.29879@gmail.com>
2026-07-17 21:28:33 +0500

    add seed script and fix prisma config

---
+++ prisma/seed.js
@@ -0,0 +1,64 @@
+import "dotenv/config";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.ts";
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+const builtInAttributes = [
+  {
+    name: "First name",
+    category: "PERSONAL_INFORMATION",
+    description: "first name of user",
+    dataType: "STRING",
+  },
+  {
+    name: "Last name",
+    category: "PERSONAL_INFORMATION",
+    description: "last name of user",
+    dataType: "STRING",
+  },{
+    name: "Location",
+    category: "PERSONAL_INFORMATION",
+    description: "location of user",
+    dataType: "STRING",
+  },{
+    name: "Personal photo",
+    category: "PERSONAL_INFORMATION",
+    description: "personal photo of user",
+    dataType: "IMAGE",
+  }
+]
+async function main() {
+  for (const attribute of builtInAttributes) {
+    const result = await prisma.attribute.upsert({
+      where: { name: attribute.name},
+      update: {
+        category: attribute.category,
+        description: attribute.description,
+        dataType: attribute.dataType,
+      },
+      create: {
+        name: attribute.name,
+        category: attribute.category,
+        description: attribute.description,
+        dataType: attribute.dataType,
+        isBuiltIn: true,
+      }
+    });
+    console.log({result});
+  } 
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    await pool.end();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    await pool.end();
+    process.exit(1);
+  });
